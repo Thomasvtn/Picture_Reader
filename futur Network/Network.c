32655056e2f434df0459a674cacda08feb272bd4
zianely <35142682+zianely@users.noreply.github.com>
2018-10-21 19:11:14 +0200

    futur network now not work

---
+++ futur Network/Network.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "reseaun.h"
+#include "Network.h"
+
+void add_layer(Network net, int size)
+{
+    if(net -> layersize > 0 )
+    init(net -> Layer,size)
+}
---
+++ futur Network/reseaun.h
@@ -0,0 +1,19 @@
+#ifndef RESEAUN_H
+#define RESEAUN_H
+
+typedef struct layer layer;
+struct layer
+{
+    int size;
+    int input_size;
+    double** weight;
+    double bias;
+};
+
+double init(layer* self, int newsize, int newinput_size);
+double** multmatricbias(double** A, double** B, int lenA, int lenB, double bias);
+double agregation(layer* self, double** data);
+double activation(layer* self);
+double forward(layer* self, double* data);
+
+#endif
